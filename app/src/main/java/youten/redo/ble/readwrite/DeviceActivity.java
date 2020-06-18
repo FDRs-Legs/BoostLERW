c6af99f5fb9cde66d19aa2b7fb720e8fd117cb97
FDRs-Legs <mentalgas@hotmail.com>
2020-06-18 10:37:56 -0400

    1st

--- app/src/main/java/youten/redo/ble/readwrite/DeviceActivity.java
+++ app/src/main/java/youten/redo/ble/readwrite/DeviceActivity.java
@@ -15,16 +15,6 @@
  */
 package youten.redo.ble.readwrite;
 
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.text.BreakIterator;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-
-import youten.redo.ble.util.BleUtil;
-import youten.redo.ble.util.BleUuid;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -35,7 +25,6 @@ import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
-import android.icu.text.DecimalFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -45,7 +34,12 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-import javax.crypto.spec.IvParameterSpec;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.UUID;
+
+import youten.redo.ble.util.BleUtil;
+import youten.redo.ble.util.BleUuid;
 
 /**
  * BLEデバイスへのconnect・Service
@@ -84,35 +78,26 @@ public class DeviceActivity extends Activity implements View.OnClickListener {
                 mConnGatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 mStatus = newState;
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        mReadBoardNameButton.setEnabled(false);
-                        mReadManufacturerNameButton.setEnabled(false);
-                        mReadSerialNumberButton.setEnabled(false);
-                        mWriteAlertLevelButton.setEnabled(false);
-                        mWriteEcoLevelButton.setEnabled(false);
-                        mReadESCFirmwareButton.setEnabled(false);
-                        mReadBatteryFirmwareButton.setEnabled(false);
-                        mReadBatteryStateButton.setEnabled(false);
-                        mReadBatteryTypeButton.setEnabled(false);
-                        mReadBatteryLevelButton.setEnabled(false);
-                        mReadOdometerButton.setEnabled(false);
-                        mReadRideStateButton.setEnabled(false);
+                runOnUiThread(() -> {
+                    mReadBoardNameButton.setEnabled(false);
+                    mReadManufacturerNameButton.setEnabled(false);
+                    mReadSerialNumberButton.setEnabled(false);
+                    mWriteAlertLevelButton.setEnabled(false);
+                    mWriteEcoLevelButton.setEnabled(false);
+                    mReadESCFirmwareButton.setEnabled(false);
+                    mReadBatteryFirmwareButton.setEnabled(false);
+                    mReadBatteryStateButton.setEnabled(false);
+                    mReadBatteryTypeButton.setEnabled(false);
+                    mReadBatteryLevelButton.setEnabled(false);
+                    mReadOdometerButton.setEnabled(false);
+                    mReadRideStateButton.setEnabled(false);
 
-                    }
-
-                    ;
                 });
             }
         }
 //playing with how to read the odometer
 
-        public int unsignedToBytes(byte b) {
-            return b & 0xFF;
-        }
-        public String toHex(byte[] arg) {
-            return String.format("%02x", arg);
-        }
+ 
 
         public String byteArrayToHexString(final byte[] bytes) {
             StringBuilder sb = new StringBuilder();
@@ -394,49 +379,76 @@ public class DeviceActivity extends Activity implements View.OnClickListener {
 
 
                     byte[] formattedOdometer = new byte[size];
-                    //for(i = 0; i <index; i++){
-                    //    formattedOdometer[i] = ascendingOdometer [(index-1)-i];
-                    //}
+                    byte[] cell10s = new byte[1];
+                    byte[] cell100s = new byte[1];
+                    byte[] cell1000s = new byte[1];
+                    byte[] cell10000s = new byte[1];
 
-                    //temp[0] = temp3[3];
 
-                    formattedOdometer[0] = ascendingOdometer[3];
+
+                    cell10000s [0]= formattedOdometer[0] = ascendingOdometer[3];
                    // formattedOdometer[0] = (byte) (formattedOdometer[0] & 0xffffffffl);
-                    formattedOdometer[1] = ascendingOdometer[2];
+                    cell1000s [0]=formattedOdometer[1] = ascendingOdometer[2];
                     //formattedOdometer[1] = (byte) (formattedOdometer[1] & 0xffffffffl);
-                    formattedOdometer[2] = ascendingOdometer[1];
-                    formattedOdometer[3] = ascendingOdometer[0];
-                   // formattedOdometer[2] = (byte) (formattedOdometer[2] & 0xffffffffl);
-                    //formattedOdometer[0] = (byte) unsignedToBytes(formattedOdometer[0]);
-                    //formattedOdometer[1] = (byte) unsignedToBytes(formattedOdometer[1]);
-                    //formattedOdometer[2] = (byte) unsignedToBytes(formattedOdometer[2]);
+                    cell100s [0]=formattedOdometer[2] = ascendingOdometer[1];
+                    cell10s [0]=formattedOdometer[3] = ascendingOdometer[0];
 
-                    //temp[3] = temp3[0];
-                    //temp3 = characteristic.getValue();
-                    //Integer temp3 = characteristic.getIntValue(0,0);
-                    //final String name2 =  name.toString();
-                    StringBuilder odometerHexString = new StringBuilder();
-                    for (byte b : formattedOdometer) {
-                        odometerHexString.append(String.format("%02X", b));
-                    }
 
-                   long odometerDecimalValue = Long.parseLong(String.valueOf(odometerHexString), 16);
+                    String finalOdometerDecimalValue =    byteArrayToHexString(ascendingOdometer);
+                    String finalOdometerDecimalValue1 =    byteArrayToHexString(cell10s);
+                    String finalOdometerDecimalValue2 =    byteArrayToHexString(cell100s);
+                    String finalOdometerDecimalValue3 =    byteArrayToHexString(cell1000s);
+                    String finalOdometerDecimalValue4 =    byteArrayToHexString(cell10000s);
+
+
+
+
+
+                  // formattedOdometer[2] = (byte) (formattedOdometer[2] & 0xffffffffl);
+                  StringBuilder cell0String =  new StringBuilder();
+                    StringBuilder cell1String =  new StringBuilder();
+                    StringBuilder cell2String =  new StringBuilder();
+                    StringBuilder cell3String =  new StringBuilder();
+                    StringBuilder cellString =  new StringBuilder();
+
+
+                    cell0String.append(String.format("%02X", cell10s[0]));
+                    cell1String.append(String.format("%02X", cell100s[0]));
+                    cell2String.append(String.format("%02X", cell1000s[0]));
+                   cell3String.append(String.format("%02X", cell10000s[0]));
+
+                   cellString.append(cell3String);
+                    cellString.append(cell2String);
+                    cellString.append(cell1String);
+                    cellString.append(cell0String);
+
+                    int odometerDecimalValue=Integer.parseInt(String.valueOf(cellString), 16);
+
+
+
 
                     double eightyMMWheels=((2 * 3.141592653589793 * 40)/1000000) ;
                     double eightyFiveMMWheels = ((2 * 3.141592653589793 * 42.5)/1000000);
                     double ninetyMMWheels = ((2 * 3.141592653589793 * 45)/1000000);
+//odometerDecimalValue = asun(odometerDecimalValue2);
+                    double odometer80 = ((odometerDecimalValue * eightyMMWheels))/4;
+                   double odometer85 = ((odometerDecimalValue * eightyFiveMMWheels))/4;
+                    double odometer90 = ((odometerDecimalValue * ninetyMMWheels)/4);
+                 String output80 = String.format("%.2f", odometer80);
+                    String output85 = String.format("%.2f", odometer85);
+                    String output90 = String.format("%.2f", odometer90);
+                  //  String finalOdometerDecimalValue = odometerDecimalValue;
 
-                    double odometer80 = ((odometerDecimalValue * eightyMMWheels)/1000000);
-                    double odometer85 = ((odometerDecimalValue * (2 * 3.141592653589793 * 45)));
-                    double odometer90 = ((odometerDecimalValue * (2 * 3.141592653589793 * 45))/1000000);
-                 String etToll = String.format("%.2f", odometer80);
-                    String etToll2 = String.format("%.2f", odometer85);
-                    String etToll3 = String.format("%.2f", odometer90);
-                    long finalOdometerDecimalValue = odometerDecimalValue;
+                  //  long finalOdometerDecimalValue1 = odometerDecimalValue2;
                     runOnUiThread(new Runnable() {
                         public void run() {
-                             mReadOdometerButton.setText( odometerHexString+"   "+etToll+" km @ 80mm"+odometerDecimalValue );
-                            setProgressBarIndeterminateVisibility(false);
+                             mReadOdometerButton.setText( output80+" km @ 80mm\n"+output85+" km @ 85mm\n"+output90+" km @ 90m");
+//                           mReadOdometerButton.setText( finalOdometerDecimalValue1+"    cells2 "+finalOdometerDecimalValue2+"   cell3 "+finalOdometerDecimalValue3+" cell 4  "+finalOdometerDecimalValue4+"  formatted  "+ OdometerDecimalValue);
+                           // mReadOdometerButton.setText( bi1+"    cells2 "+bi2+"   cell3 "+bi3+" cell 4  "+bi4+"  formatted  ");
+                            //mReadOdometerButton.setText( cell3String +":"+cell2String +":"+cell1String +":"+cell0String +"  formatted  "+decimal);
+
+
+
                         }
 
                         ;
@@ -494,29 +506,29 @@ public class DeviceActivity extends Activity implements View.OnClickListener {
 
         // state
         mStatus = BluetoothProfile.STATE_DISCONNECTED;
-        mReadBoardNameButton = (Button) findViewById(R.id.read_board_name_button);
+        mReadBoardNameButton = findViewById(R.id.read_board_name_button);
         mReadBoardNameButton.setOnClickListener(this);
-        mReadManufacturerNameButton = (Button) findViewById(R.id.read_manufacturer_name_button);
+        mReadManufacturerNameButton = findViewById(R.id.read_manufacturer_name_button);
         mReadManufacturerNameButton.setOnClickListener(this);
-        mReadSerialNumberButton = (Button) findViewById(R.id.read_serial_number_button);
+        mReadSerialNumberButton = findViewById(R.id.read_serial_number_button);
         mReadSerialNumberButton.setOnClickListener(this);
-        mReadESCFirmwareButton = (Button) findViewById(R.id.read_esc_firmware_button);
+        mReadESCFirmwareButton = findViewById(R.id.read_esc_firmware_button);
         mReadESCFirmwareButton.setOnClickListener(this);
-        mReadBatteryFirmwareButton = (Button) findViewById(R.id.read_battery_firmware_button);
+        mReadBatteryFirmwareButton = findViewById(R.id.read_battery_firmware_button);
         mReadBatteryFirmwareButton.setOnClickListener(this);
-        mReadBatteryStateButton = (Button) findViewById(R.id.read_battery_state_button);
+        mReadBatteryStateButton = findViewById(R.id.read_battery_state_button);
         mReadBatteryStateButton.setOnClickListener(this);
-        mReadBatteryLevelButton = (Button) findViewById(R.id.read_battery_level_button);
+        mReadBatteryLevelButton = findViewById(R.id.read_battery_level_button);
         mReadBatteryLevelButton.setOnClickListener(this);
-        mReadBatteryTypeButton = (Button) findViewById(R.id.read_battery_type_button);
+        mReadBatteryTypeButton = findViewById(R.id.read_battery_type_button);
         mReadBatteryTypeButton.setOnClickListener(this);
-        mWriteEcoLevelButton = (Button) findViewById(R.id.write_eco_level_button);
+        mWriteEcoLevelButton = findViewById(R.id.write_eco_level_button);
         mWriteEcoLevelButton.setOnClickListener(this);
-        mWriteAlertLevelButton = (Button) findViewById(R.id.write_alert_level_button);
+        mWriteAlertLevelButton = findViewById(R.id.write_alert_level_button);
         mWriteAlertLevelButton.setOnClickListener(this);
         mReadOdometerButton = (Button) findViewById(R.id.read_odometer_button);
         mReadOdometerButton.setOnClickListener(this);
-        mReadRideStateButton = (Button) findViewById(R.id.read_ride_state_button);
+        mReadRideStateButton = findViewById(R.id.read_ride_state_button);
         mReadRideStateButton.setOnClickListener(this);
     }
 
